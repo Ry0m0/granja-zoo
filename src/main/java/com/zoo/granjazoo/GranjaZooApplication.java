c70f37773abd7b73d730a854e70edaaaaa9831e9
Ruoyan <hanhanlianhongya@gmail.com>
2026-05-12 11:10:11 +0200

    Initial commit: Spring Boot project structure

---
+++ src/main/java/com/zoo/granjazoo/GranjaZooApplication.java
@@ -0,0 +1,13 @@
+package com.zoo.granjazoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GranjaZooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GranjaZooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoo/granjazoo/GranjaZooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoo.granjazoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GranjaZooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
